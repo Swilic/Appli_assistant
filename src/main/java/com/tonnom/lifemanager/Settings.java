bfa5fde866cf98c128cb0c347ed5eee6f94c103a
unknown <68517@etu.he2b.be>
2026-03-16 00:13:47 +0100

    Ajout des 1er idées et Fonction pour la conception de la fonctionnalitées Setting

--- src/main/java/com/tonnom/lifemanager/Settings.java
+++ src/main/java/com/tonnom/lifemanager/Settings.java
@@ -1,4 +1,7 @@
 package com.tonnom.lifemanager;
 
+//Pour le moment on gerera que 2 thèmes pour l'appli : clair et sombre
+
+
 public class Settings {
 }
---
+++ src/main/java/com/tonnom/lifemanager/controllers/SettingController.java
@@ -0,0 +1,8 @@
+package com.tonnom.lifemanager.controllers;
+
+//Pour le moment on gerera que 2 thèmes pour l'appli : clair et sombre
+
+public class SettingController {
+
+    public void theme_zone() {} //fct qui me permettra d'ouvrir une nouvelle page donnant la possibilité a l'user de choisir un theme
+}
